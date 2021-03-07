250dc05c7c4fac9cbb6484d1c5264de65e7d0654
EC2 Default User <ec2-user@ip-172-31-34-55.us-east-2.compute.internal>
2021-03-07 05:12:47 +0000

    part 2 done

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -24,10 +24,20 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: PG-13, G
   # enter step to "submit" the search form on the homepage
+  And I press "Refresh" button
   # enter step(s) to ensure that PG and R movies are visible
+  #Then there should be 6 movies that are PG
+  Then I should see this The Terminator, When Harry Met Sally, Amelie, The Incredibles, Raiders of the Lost Ark
   # enter step(s) to ensure that other movies are not visible
+  #Then there should be 0 movies that are ["PG-13", "G"]
+  Then I should not see this Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run
 
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: PG, PG-13, R, G
+  And I press "Refresh" button
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -28,14 +28,67 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+When /I press "(.*)" button/ do |button|
+  click_button(button)
+end
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(', ')
+  ratings.each do |rating|
+    uncheck ? uncheck("ratings[#{rating}]") : (check("ratings[#{rating}]"))
+  end
+  #@movie = Movie.where(rating_list)
+  #end
+end
+
+#When ('I uncheck (.*)') do |rating_list|
+#    rating_list.each do | rating1 |
+#      check(rating1)
+#    end
+#  #fail "Unimplemented"
+#end
+
+Then /there should be (.*) movies that are (.*)/ do |n, rating_list|
+  count = Movie.where(rating: rating_list).count
+  #ratings = rating_list.split(',')
+  #ratings.each do |rating|
+   # count = count + Movie.where("ratings[#{rating}]").count
+  #end
+  #count.should be n.to_i
+  puts Movie.with_ratings(rating_list)["title"]
+end
+
+Then /^(?:|I )should see this (.*)/ do |text|
+  text1 = text.split(', ')
+  text1.each do |text2|
+    #if page.respond_to? :should
+    page.should have_content(text2)
+    #else
+     # assert page.has_content?(text2)
+    #end
+  end
+end
+
+Then /^(?:|I )should not see this (.*)/ do |text|
+  text1 = text.split(', ')
+  text1.each do |text2|
+    #if page.respond_to? :should
+    page.should have_no_content(text2)
+    #else
+     # assert page.has_content?(text2)
+    #end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #@movie = Movie.all
+  Movie.all.each do |movie|
+    page.should have_content(movie["title"])
+  end
+  #expect(rows).to eq value 10
+  #fail "Unimplemented"
 end
